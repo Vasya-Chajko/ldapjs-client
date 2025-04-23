a55dd081a209d2cea1a8d0895e1bcc9a268ff55a
Вася Чайко <vasya@chajko.ru>
2025-04-23 11:52:40 +0700

    Parse messages iterative

--- src/responses/parser.js
+++ src/responses/parser.js
@@ -23,51 +23,63 @@ class Parser extends EventEmitter {
     super();
     this.buffer = null;
   }
-
+  
+  parseLoop(buffer) {
+    while(true) {
+      const ber = new BerReader(buffer);
+      
+      try {
+        ber.readSequence();
+      } catch (e) {
+        this.emit('error', e);
+        return;
+      }
+      
+      let nextMessages = null;
+      if (ber.remain > ber.length) {
+        nextMessages = buffer.slice(ber.offset + ber.length);
+        ber._size = ber.offset + ber.length;
+        assert.equal(ber.remain, ber.length);
+      }
+      
+      try {
+        const message = getMessage(ber);
+        message.parse(ber);
+        this.emit('message', message);
+      } catch (e) {
+        this.emit('error', e);
+      }
+      
+      if (nextMessages === null) {
+        break;
+      } else {
+        buffer = nextMessages;
+      }
+    }
+  }
+  
   parse(data) {
     assert.buffer(data, 'data');
-
+    
     this.buffer = this.buffer ? Buffer.concat([this.buffer, data]) : data;
-
+    
     const ber = new BerReader(this.buffer);
-
+    
     try {
       ber.readSequence();
     } catch (e) {
       this.emit('error', e);
       return;
     }
-
-    // If ber.length == 0, then we do not have a complete chunk
-    // and can't proceed with parsing.
-    // Allowing this function to continue results in an infinite loop
-    // and due to the recursive nature of this function quickly 
-    // hits the stack call size limit.
-    // This only happens with very large responses.
+    
+    // If the chunk is not full, we continue accumulating the buffer.
     if (ber.remain < ber.length || ber.length === 0) {
       return;
     }
-
-    let nextMessage = null;
-    if (ber.remain > ber.length) {
-      nextMessage = this.buffer.slice(ber.offset + ber.length);
-      ber._size = ber.offset + ber.length;
-      assert.equal(ber.remain, ber.length);
-    }
-
+    
+    const messages = this.buffer;
     this.buffer = null;
-
-    try {
-      const message = getMessage(ber);
-      message.parse(ber);
-      this.emit('message', message);
-    } catch (e) {
-      this.emit('error', e);
-    }
-
-    if (nextMessage) {
-      this.parse(nextMessage);
-    }
+    this.parseLoop(messages);
   }
 }
 
